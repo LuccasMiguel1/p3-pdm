63a98f230214fc26977d7557cff627bb2e9e9f00
LuccasMiguel1 <lumi137@hotmail.com>
2026-06-19 17:06:35 -0300

    Feat(p3): Integra API de gatos para busca de imagens

--- p3-pdm/App.tsx
+++ p3-pdm/App.tsx
@@ -1,3 +1,49 @@
-export default function App() {
-    return null;
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+interface Gato {
+  id: string;
+  url: string;
 }
+
+export default function App() {
+    const [gatos, setGatos] = useState<Gato[]>([]);
+
+
+  const buscarGatos = async () => {
+
+    const resposta = await fetch(
+      'https://api.thecatapi.com/v1/images/search?limit=5'
+    );
+
+
+    const dados: Gato[] = await resposta.json();
+
+
+    setGatos(dados);
+
+  };
+
+
+  return (
+
+    <View style={styles.container}>
+
+      
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    alignItems:'center',
+    backgroundColor:'#494c4e'
+  },
+
+});
